4805ae632853b265a1f29b0084dffc153c867753
Desmond <andro.mieli2001@gmial.com>
2021-10-13 00:01:33 +0200

    [HF] No recent search info fix
    No recent search info remains after user go back to the previous page instead of disappearing -- fix

--- src/scripts/anchor.js
+++ src/scripts/anchor.js
@@ -2,7 +2,10 @@ function setAnchor(pageName) {
 
   let url = window.location.href.split('?=', 2)[1];
 
-  if (pageName != url) history.pushState(null, '', '?=' + pageName);
+  if (pageName != url) {
+    history.pushState(null, '', '?=' + pageName);
+  }
+  console.log('page name: ', pageName + '\nurl: ', url);
 }
 window.addEventListener('popstate', function (event) {
   let newUrl = window.location.href.split('?=', 2)[1];
@@ -14,6 +17,7 @@ window.addEventListener('popstate', function (event) {
     $('.search-page').show();
     $('.my-search-page').hide();
     $('.my-search-history').hide();
+    $('.no-recent-search').hide();
     $('.live-search-container').hide();
     $('.manualShift-page').hide();
     $('.single-page').hide();
@@ -27,6 +31,7 @@ window.addEventListener('popstate', function (event) {
     $('.search-page').hide();
     $('.my-search-page').hide();
     $('.my-search-history').hide();
+    $('.no-recent-search').hide();
     $('.live-search-container').hide();
     $('.manualShift-page').hide();
     $('.single-page').hide();
@@ -40,6 +45,7 @@ window.addEventListener('popstate', function (event) {
     $('.search-page').show();
     $('.my-search-page').hide();
     $('.my-search-history').hide();
+    $('.no-recent-search').hide();
     $('.live-search-container').hide();
     $('.manualShift-page').hide();
     $('.single-page').hide();
@@ -63,6 +69,7 @@ window.addEventListener('popstate', function (event) {
     $('.search-page').hide();
     $('.my-search-page').hide();
     $('.my-search-history').hide();
+    $('.no-recent-search').hide();
     $('.live-search-container').hide();    
     $('.single-page').hide();
   }
